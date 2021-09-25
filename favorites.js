38b714e62b8ee01941343bae3a287fea6bb08fd1
Nick Kulway <nkulway@gmail.com>
2021-09-25 12:20:09 -0400

    fixed image height on cards

--- favorites.js
+++ favorites.js
@@ -1,5 +1,17 @@
 const favoritesList = document.querySelector(".favorites");
 
+const randomImg = new Array();
+
+randomImg[0] = "https://images.unsplash.com/photo-1555658636-6e4a36218be7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80"
+randomImg[1] = "https://images.unsplash.com/photo-1528823872057-9c018a7a7553?ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8YnJld2VyeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+randomImg[2] = "https://images.unsplash.com/photo-1524587253133-266042ff67d0?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YnJld2VyeXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+
+function displayRandom(){
+let number = Math.floor(Math.random()*randomImg.length); 
+return number;
+};
+console.log(displayRandom())
+
 document.addEventListener("DOMContentLoaded", (e) => {
     e.preventDefault();
     //gets favorites from the local storage and then converts to an array
@@ -8,7 +20,7 @@ document.addEventListener("DOMContentLoaded", (e) => {
     //creates an HTML element for each brewery in the local storage
     const favoritesArray = favorites.map((favorite) => {
         return `<div class="card clear" data-name = "${favorite.name}" style=" background-color: #f2c025; width: 18rem; box-shadow: 3px 3px 3px;">
-        <img class="card-img-top" src="https://images.unsplash.com/photo-1555658636-6e4a36218be7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80" alt="Card image cap">
+        <img class="card-img-top" src="${randomImg[displayRandom()]}" alt="Card image cap" style="height: 250px;">
     <div class="card-body d-flex flex-column ">
     <a style ="text-decoration: none;" href="${favorite.website_url}" target="_blank" class="card-link">
       <h5 class="card-title">${favorite.name}</h5>
